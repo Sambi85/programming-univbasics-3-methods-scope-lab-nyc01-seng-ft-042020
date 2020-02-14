54a17fa657e9d0fde4240c75d68c5acc7dcbda26
Samuel Lesser <ineedmoreplates@gmail.com>
2020-02-14 03:14:19 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -12,6 +12,7 @@ status = 'Thank You Mario! But Our Princess Is In Another Castle!'
 end
 
 def link 
+<<<<<<< HEAD
   phrase = "It's Dangerous To Go Alone! Take This.\n"
     puts phrase
       phrase
@@ -21,3 +22,9 @@ def any_phrase(phrase)
   phrase = "Do A Barrel Roll!\n"
     puts phrase
 end 
+=======
+  phrase = "It's Dangerous To Go Alone! Take This"
+    puts phrase
+      phrase
+end
+>>>>>>> 52dc9303bf0a0a533833d1d58073a8d9c32edf1c
